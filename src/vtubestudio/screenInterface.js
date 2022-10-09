6f8e351baba70e052cc72a04ff10f96af6c0ffd4
Renpona <renponaunit6@gmail.com>
2022-10-08 23:55:17 -0400

    prefix change

--- src/vtubestudio/screenInterface.js
+++ src/vtubestudio/screenInterface.js
@@ -1,10 +1,14 @@
 export class ScreenInterface {
+    // count of times I've forgotten to set this properly: 0
+    static productionMode = false;
+
     static findArtMesh (number) {
         let index = number + 1;
         let row = Math.ceil(index / 40);
         let column = index - (40 * (row - 1));
 
-        let artMesh = `A${column}x${row}`;
+        // I realized Z was a better prefix, but it's a pain to redo the early demo files that used A
+        let artMesh = productionMode ? `Z${column}x${row}` : `A${column}x${row}`;
         return artMesh;
     }
 }
