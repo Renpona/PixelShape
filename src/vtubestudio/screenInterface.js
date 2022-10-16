a053b277d93f0bd17b05ce09bb2dd93a426b1495
Renpona <renponaunit6@gmail.com>
2022-10-16 00:59:08 -0400

    beta updates

--- src/vtubestudio/screenInterface.js
+++ src/vtubestudio/screenInterface.js
@@ -8,7 +8,7 @@ export class ScreenInterface {
         let column = index - (40 * (row - 1));
 
         // I realized Z was a better prefix, but it's a pain to redo the early demo files that used A
-        let artMesh = productionMode ? `Z${column}x${row}` : `A${column}x${row}`;
+        let artMesh = this.productionMode ? `Z${column}x${row}` : `A${column}x${row}`;
         return artMesh;
     }
 }
